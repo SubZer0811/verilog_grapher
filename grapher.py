38c1afd0399a110ce1a21987c8ab2d34a14281a3
Subash Mylraj <subashmylraj@gmail.com>
2020-10-26 22:51:11 +0300

    Completed grapher!

--- grapher.py
+++ grapher.py
@@ -8,19 +8,32 @@ nodes: gates; edges; connections
 '''
 in_n, out_n, nodes, edges = verilog_parser.parser("c17.v")     
 
-
 G=nx.DiGraph()
-G.add_nodes_from(in_n)
+G.add_nodes_from(in_n, node_color="r")
 G.add_nodes_from(out_n)
 G.add_nodes_from(nodes)
 
+colour_map = []
+size = []
+line_size = []
+for node in G:
+	if node in in_n:
+		colour_map.append('red')
+	elif node in out_n:
+		colour_map.append('green')
+	else:
+		colour_map.append('blue')
+	size.append(100*len(node))
+
 for i in edges:
-    G.add_edge(*edges[i])
+	for j in i[2]:
+		G.add_edge(i[1], j, weight=6)
+		# line_size.append(3)
 
 print("Nodes of graph: ")
 print(G.nodes())
 print("Edges of graph: ")
 print(G.edges())
-nx.draw_networkx(G, with_labels=True)
+nx.draw(G, with_labels=True, node_color=colour_map, node_size=size, arrowsize=20, pos=nx.spring_layout(G, k=7))
 plt.savefig("path_graph1.png")
 plt.show()
