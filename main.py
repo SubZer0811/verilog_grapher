cc60051ee56e3df46049e98e17d82842d4c8b7a9
Subash Mylraj <subashmylraj@gmail.com>
2020-10-27 21:55:18 +0300

    Added support for verbose mode

--- main.py
+++ main.py
@@ -1,8 +1,14 @@
+import argparse
 from verilog_parser import parser
 from grapher import grapher
 
 if __name__ == "__main__":
 	
-	file_ = "c17.v"
-	in_n, out_n, nodes, edges = parser(file_)
-	grapher(in_n, out_n, nodes, edges)
+	cli = argparse.ArgumentParser(description="Convert Gate level verilog to a visualizable graph")
+	cli.add_argument('verilog_file', type=str, help="Path to the verilog file")
+	cli.add_argument('--verbose', type=bool, default=0, help="Display information about nodes and edges of graph")
+
+	args = cli.parse_args()
+
+	in_n, out_n, nodes, edges = parser(args.verilog_file, verbose=args.verbose)
+	# grapher(in_n, out_n, nodes, edges)
